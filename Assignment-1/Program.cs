c221115f56507973719c1d0a76a30accbf2fb2e2
Jorjo6712 <kiro.nicolov@gmail.com>
2024-05-13 17:50:24 +0200

    yay

---
+++ Assignment-1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+namespace Assignment1 {
+    internal class Assignment1 {
+        private static void Main(string[] args) 
+        {
+            Assignment1 assignment1 = new Assignment1();
+            Thread tempGen = new Thread(new ThreadStart(assignment1.TempGen));
+            tempGen.Priority = ThreadPriority.BelowNormal;
+            tempGen.Start();
+            
+        }
+
+        internal void TempGen() {
+            sbyte temp;
+            sbyte minTemp = 0;
+            sbyte maxTemp = 100;
+            byte amountAlarmsTriggered = 1;
+            byte maxAlarmTriggers = 3;
+
+            Random randomTemp = new Random();
+
+            while(true) {
+                Thread.Sleep(2000);
+                temp = (sbyte)randomTemp.Next(-20,120);
+                Console.WriteLine(temp);
+                if(minTemp > temp || maxTemp <= temp)
+                {
+                    Console.WriteLine("Alarm: the temperature has gone into invalid range!");
+                    
+                    amountAlarmsTriggered++;
+                }
+                if (amountAlarmsTriggered > maxAlarmTriggers) {
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Practice-2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Practice2
+{
+    internal class Practice2 {
+        private static void Main(string[] args) {
+            var practice2 = new Practice2();            
+            Thread task1Thread = new Thread(new ThreadStart(practice2.Task1));
+            Thread task2Thread = new Thread(new ThreadStart(practice2.Task2));
+            task1Thread.Start();
+            Thread.Sleep(1000);
+            task2Thread.Start();
+        }
+
+        internal void Task1() {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("C#-trådning er nemt!");
+
+            }
+        }
+
+        internal void Task2() {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Også med flere tråde …");
+            }
+        }
+    }
+}
